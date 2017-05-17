a70348c8b98340cb5a38e0a185a370573fc9baf8
Nedu <nedumrobert@gmail.com>
2017-05-17 22:42:02 +0100

    Revamped portfolio to a single page website

---
+++ scripts/nedu.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+  var scroll_start = 0;
+  var startchange = $('#webdev');
+  var offset = startchange.offset();
+  if (startchange.length){
+    $(document).scroll(function() {
+      scroll_start = $(this).scrollTop();
+      if(scroll_start > offset.top) {
+          $(".navbar-inverse").addClass("scrolled");
+      } else {
+          $('.navbar-inverse').removeClass("scrolled");
+      }
+    });
+  }
+});
+
+
